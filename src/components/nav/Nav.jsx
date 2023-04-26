35820598a231a6fa9145b8619ffbaaa8c719cc7c
santhosh62372n <126991013+santhosh62372n@users.noreply.github.com>
2023-04-26 00:45:27 -0400

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Nav() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(
+        "https://newsapi.org/v2/everything?q=bitcoin&from=2023-03-26&sortBy=publishedAt&apiKey=09dfed00446e43cc9362f6a575c68a72"
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+
+          const startSlice = Math.random(100);
+          const endSlice = startSlice + 5;
+          setData(res.articles.slice(startSlice, endSlice));
+        });
+    }, 6 * 1000);
+  }, []);
+
   return (
-    <div id="posts_section">
-      <h1>Nav</h1>
+    <div id="nav">
+      <ul>
+        {data.map(({ author, content, description, title }) => (
+          <li>
+            <p>{author}</p>
+            <p>{description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
